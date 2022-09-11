fce4fffb6a269684f7990053da0311977c4dada0
Ben Kinsey <bkinsey@gmail.com>
2022-09-11 13:37:41 -1000

    add prettier

--- src/functions/listBooks.ts
+++ src/functions/listBooks.ts
@@ -1,18 +1,18 @@
 export const handler = async () => {
   return [
     {
-      id: "abc-123",
+      id: 'abc-123',
       title: "The Hitchhiker's Guide to the Galaxy",
       completed: true,
       rating: 10,
-      reviews: ["This book is awesome!"],
+      reviews: ['This book is awesome! What a great read!'],
     },
     {
-      id: "def-456",
-      title: "The Restaurant at the End of the Universe",
+      id: 'def-456',
+      title: 'The Restaurant at the End of the Universe',
       completed: false,
       rating: 2,
-      reviews: ["This book is terrible!"],
+      reviews: ['This book is terrible!'],
     },
   ];
 };
